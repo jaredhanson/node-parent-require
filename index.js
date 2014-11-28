6772edd888da0fd23b8a9f46d5c82ab91f26367c
Lukasz Sielski <lukaszsielski@gmail.com>
2014-11-28 22:41:27 +0000

    Possibility to get root dependency

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ module.exports = function(id) {
   }
   throw new Error("Cannot find module '" + id + "' from parent");
 };
+
+/**
+ * Returns module from top parent. Fallbacks to self
+ */
+module.exports.reverse = function ( id ) {
+
+    var stack = [module];
+    var parent = module.parent;
+    for ( ; parent ; parent = parent.parent ) {
+        stack.push(parent);
+    }
+    var current;
+    while((current = stack.pop()) != null) {
+        try {
+            return current.require(id);
+        } catch(ex) {}
+    }
+    throw new Error("Cannot find module '" + id + "' from parent");
+}
